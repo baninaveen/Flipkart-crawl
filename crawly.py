e77d63dc1b9954a2901991a558f18b865a3b0f1e
Ankit Kachroo <ankitkachru@users.noreply.github.com>
2016-05-05 11:37:22 +0530

    Optimized crawly

--- crawly.py
+++ crawly.py
@@ -1,34 +1,30 @@
-from bs4 import BeautifulSoup
-import requests
-import subprocess
-import os,sys
-import time
+from bs4 import BeautifulSoup as bs
+import requests as rq
 from urlparse import urlparse
 
 def prodfind(url):
 	parsed = urlparse(url)
 	parsed_url = "http://"+parsed.netloc+parsed.path
 	print parsed_url
-	r = requests.get(parsed_url)
+	r = rq.get(parsed_url)
 	data = r.text
-	soup = BeautifulSoup(data)
+	soup = bs(data)
+	
+	for price in soup.find_all(itemprop="price"):
+		p = price.get('content')
+		p = p.replace(',','')
+		print "Price:",int(p)
 	
 	for name in soup.find_all(itemprop="name"):
 		n = name.get_text()
 		#n = n.replace(',','')
 		print "Name:", n
 	
-	for price in soup.find_all(itemprop="price"):
-		p = price.get('content')
-		p = p.replace(',','')
-		print "Price:",int(p)
-	       
-	
         for img in soup.find_all(class_ ="productImage"):
 		i = img.get('src')
 		#n = n.replace(',','')
 		print "Image: ",i
 		break
-               
+
 url = raw_input("Paste flipkart URL here: ")
 prodfind(url)
